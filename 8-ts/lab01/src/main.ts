69d551edc29666527c692b1ee2f59b2f5dc934e1
Daniel Leal <dls@icomp.ufam.edu.br>
2024-09-26 18:20:32 -0400

    Adicionando descrição do laboratório

--- 8-ts/lab01/src/main.ts
+++ 8-ts/lab01/src/main.ts
@@ -1,4 +1,8 @@
-let username = 'Daniel'
-console.log(username)
+let username = "Daniel";
+console.log(username);
 
-
+// Uso de tipos com TypeScript
+/* Criar uma pequena app "Lembretes / To Do List"
+O usuário deve ser capaz de criar/inserir, editar e apagar pequenos lembretes
+para realizar atividades pequenas
+Cada lembrete deve ter título, data e hora de inserção */
