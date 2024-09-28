c90b91e2d5038e126aea65f919baa968ecbdca2e
Daniel Leal <dls@icomp.ufam.edu.br>
2024-09-27 21:27:35 -0400

    adicionando correções

--- 8-ts/lab01/src/main.ts
+++ 8-ts/lab01/src/main.ts
@@ -1,8 +1,9 @@
-let username = "Daniel";
-console.log(username);
+let username = 'Daniel'
+console.log(username)
 
-// Uso de tipos com TypeScript
-/* Criar uma pequena app "Lembretes / To Do List"
-O usuário deve ser capaz de criar/inserir, editar e apagar pequenos lembretes
-para realizar atividades pequenas
-Cada lembrete deve ter título, data e hora de inserção */
+const img = document.querySelector('img')!
+const myImg = document.getElementById('#img') as HTMLImageElement
+const nextImg = <HTMLImageElement>document.querySelector('#img') as HTMLImageElement
+
+img.src
+myImg.src
